4ba4b52311d53e329fe220beed201296704030e7
vilparesh <paresh.bavishi@emxcelsolutions.com>
2021-03-11 17:49:31 +0530

    ec2 module added with vpc

--- main.tf
+++ main.tf
@@ -8,4 +8,9 @@ module "vpc" {
   cidrs_public_subnet  = var.cidrs_public_subnet
   cidrs_private_subnet = var.cidrs_private_subnet
 }
-
+module "ec2" {
+  source = "./ec2"
+  instance_pub_key = "/home/paresh/paresh/.ssh/id_rsa.pub"
+  security_group = "module.vpc.vpc_security_group"
+  public_subnet = "module.vpc.vpc_pub_subnet"
+}
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -6,3 +6,10 @@ output "vpc_internet_gateway" {
   value = "${aws_internet_gateway.emxcel_gateway.id}"
 }
 
+output "vpc_security_group" {
+  value = "${aws_security_group.emxcel_sec_group.id}"
+}
+
+output "vpc_pub_subnet" {
+  value = "${aws_subnet.emxcel_public_subnet.*.id}"
+}
